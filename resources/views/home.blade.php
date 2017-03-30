7fe7588e64b9b8dd4661859f0e0b2f11708bce5c
Anlanda1 <angellanda22@gmail.com>
2017-03-30 08:33:33 -0600

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">BIenvenido usuario</div>
+
+                <div class="panel-body">
+                    Estás registrado.
+                </div>
+                <div>
+                    <IMG SRC="gokuu.jpg" ALT="Hola">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
